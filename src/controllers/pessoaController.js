345368bf1a94ae3fa9369f48ee5b1e9eef472fd3
Thalles José <thallesjosesv@gmail.com>
2025-09-01 16:50:34 -0300

    migração de dados e iniciando routes/controllers

---
+++ src/controllers/pessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+    static async getAll(next,req,res){
+        try{
+
+        }catch(erro){
+            //erro
+        }
+    }
+}
+
+module.exports = PessoaController;
--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -22,6 +22,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Pessoa',
+    tableName; 'pessoas'
   });
   return Pessoa;
 };
