fbb2745a5acc1da6a414465737075510c9feb48b
Thalles José <thallesjosesv@gmail.com>
2025-09-11 19:26:01 -0300

    create:migrations_and_models

---
+++ src/migrations/20250911211941-create-curso.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cursos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        type: Sequelize.STRING
+      },
+      descricao: {
+        type: Sequelize.STRING
+      },
+      docente_id: {
+        allowNull: false,
+        typle: Sequelize.INTEGER,
+        references: { model: 'pessoas', key: 'id' },
+      },
+      categoria_id: {
+        allowNull: false,
+        typle: Sequelize.INTEGER,
+        references: { model: 'categorias', key: 'id' },
+      },
+      data_inicio: {
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cursos');
+  }
+};
---
+++ src/models/categoria.js
@@ -0,0 +1,19 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Categoria extends Model {
+    static associate(models) {
+      Categoria.hasMany(models.Curso,)
+    }
+  }
+  Categoria.init({
+    titulo: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Categoria',
+    tableName: 'categorias'
+  });
+  return Categoria;
+};
